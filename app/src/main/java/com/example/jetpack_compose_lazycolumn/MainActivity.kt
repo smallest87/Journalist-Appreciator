a24062b7f13ce9ab9274437b9fe5748e968d0a81
Julian Sukrisna <smallest87@gmail.com>
2023-07-30 12:32:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jetpack_compose_lazycolumn/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.jetpack_compose_lazycolumn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpack_compose_lazycolumn.ui.theme.Jetpack_Compose_LazyColumnTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Jetpack_Compose_LazyColumnTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyApp() {
+    Scaffold(
+        content = {
+            BarkHomeContent()
+        }
+    )
+}
---
+++ app/src/main/java/com/example/jetpack_compose_lazycolumn/data/Puppy.kt
@@ -0,0 +1,10 @@
+package com.example.jetpack_compose_lazycolumn.data
+
+data class Puppy(
+    val id: Int,
+    val title: String,
+    val sex: String,
+    val age: Int,
+    val description: String,
+    val puppyImageId: Int = 0
+)
