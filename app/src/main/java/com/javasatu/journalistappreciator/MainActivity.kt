86a97dafbac930f142907248e4748cc9aa9fe848
Julian Sukrisna <smallest87@gmail.com>
2023-08-16 18:06:02 +0700

    Update MainActivity.kt

--- app/src/main/java/com/javasatu/journalistappreciator/MainActivity.kt
+++ app/src/main/java/com/javasatu/journalistappreciator/MainActivity.kt
@@ -2,23 +2,14 @@ package com.javasatu.journalistappreciator
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import com.javasatu.journalistappreciator.contants.DATABASE_NAME
@@ -26,7 +17,6 @@ import com.javasatu.journalistappreciator.data.User
 import com.javasatu.journalistappreciator.data.UserDao
 import com.javasatu.journalistappreciator.data.UserDatabase
 import com.javasatu.journalistappreciator.ui.theme.LazyColumnTheme
-import com.javasatu.journalistappreciator.view.HeaderAplikasi
 import com.javasatu.journalistappreciator.view.TampilanAwal
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -99,7 +89,6 @@ class MainActivity : ComponentActivity() {
 
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     private fun PraTampilanAwal() {
         Scaffold(
